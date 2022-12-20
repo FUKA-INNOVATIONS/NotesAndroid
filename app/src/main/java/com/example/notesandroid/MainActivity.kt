df72977efaea9f4a757c8b0bfaf3e5f13cf8ac67
Fuwad Kalhori <fuad.kalhori@gmail.com>
2022-12-20 12:01:15 +0200

    add and remove notes

--- app/src/main/java/com/example/notesandroid/MainActivity.kt
+++ app/src/main/java/com/example/notesandroid/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.notesandroid.data.NotesDataSource
+import com.example.notesandroid.model.Note
 import com.example.notesandroid.screen.NoteScreen
 import com.example.notesandroid.ui.theme.NotesAndroidTheme
 
@@ -17,17 +21,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAndroidTheme {
+                val notes = remember {
+                    mutableStateListOf<Note>()
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    NoteScreen()
+                    NoteScreen(notes = notes,
+                        onNoteAdd = {
+                            notes.add(it)
+                        },
+                        onRemoveNote = {
+                            notes.remove(it)
+                        })
                 }
             }
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/notesandroid/screen/NoteScreen.kt
+++ app/src/main/java/com/example/notesandroid/screen/NoteScreen.kt
@@ -1,29 +1,41 @@
 package com.example.notesandroid.screen
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.notesandroid.R
 import com.example.notesandroid.components.NoteButton
 import com.example.notesandroid.components.NoteInputText
+import com.example.notesandroid.data.NotesDataSource
+import com.example.notesandroid.model.Note
+import java.time.format.DateTimeFormatter
 
 @Composable
-fun NoteScreen() {
+fun NoteScreen(
+    notes: List<Note>,
+    onNoteAdd: (Note) -> Unit,
+    onRemoveNote: (Note) -> Unit
+) {
     var title by remember { mutableStateOf(value = "") }
-
     var description by remember { mutableStateOf(value = "") }
+    val context = LocalContext.current
 
     Column(modifier = Modifier.padding(6.dp)) {
         TopAppBar(title = {
@@ -55,7 +67,49 @@ fun NoteScreen() {
                 })
             NoteButton(
                 text = "Save",
-                onClick = { /*TODO*/ })
+                onClick = {
+                    if (title.isNotEmpty() && description.isNotEmpty()) {
+                        onNoteAdd(Note(title = title, description = description))
+                        title = ""
+                        description = ""
+                        Toast.makeText(context, "Note added", Toast.LENGTH_SHORT)
+                    }
+                })
+        }
+        Divider(modifier = Modifier.padding(10.dp))
+        LazyColumn {
+            items(notes) { note ->
+                NoteRow(note = note, onNoteClicked = {
+                    onRemoveNote(note)
+                } )
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onNoteClicked: (Note) -> Unit
+) {
+    Surface(
+        modifier = modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
+            .fillMaxWidth(),
+        color = Color(0xFFDFE6EB),
+        elevation = 6.dp
+    ) {
+        Column(
+            modifier
+                .clickable { onNoteClicked(note) }
+                .padding(horizontal = 14.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = note.title, style = MaterialTheme.typography.subtitle2)
+            Text(text = note.description, style = MaterialTheme.typography.subtitle1)
+            Text(text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE, d MMM")), style = MaterialTheme.typography.caption)
         }
     }
 }
@@ -63,5 +117,5 @@ fun NoteScreen() {
 @Preview(showBackground = true)
 @Composable
 fun NotesScreenPreview() {
-    NoteScreen()
+    NoteScreen(notes = NotesDataSource().loadNotes(), onNoteAdd = {}, onRemoveNote = {})
 }
